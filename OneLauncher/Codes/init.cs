5a0dba62714df46f06a0fbb5b3e4d6bacf828385
Lnetface <elictop@icloud.com>
2025-05-05 02:13:18 +0800

    优化了代码

--- OneLauncher/Codes/init.cs
+++ OneLauncher/Codes/init.cs
@@ -1,23 +1,37 @@
 ﻿using OneLauncher.Core;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
-namespace OneLauncher.Codes
+namespace OneLauncher.Codes;
+public static class Init
 {
-    public static class Init
+    public static string BasePath { get; private set; }
+    public static DBManger ConfigManger { get; private set; }
+    public static bool IsNetwork { get; private set; }
+    public static async Task Initialize()
     {
-        public static string BasePath { get; private set; }
-        public static DBManger ConfigManger { get; private set; }
-
-        public static async Task Initialize()
+        // 检查网络连接状态
+        try
         {
-            // 初始化 BasePath
-            BasePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/OneLauncher/";
-            Directory.CreateDirectory(BasePath); // 确保目录存在
-
-            // 初始化 ConfigManger
-            ConfigManger = new DBManger(new AppConfig(), BasePath);
+            using (var ping = new Ping())
+            {
+                var reply = await ping.SendPingAsync("microsoft.com", 1000); 
+                IsNetwork = reply.Status == IPStatus.Success;
+            }
         }
+        catch
+        {
+            IsNetwork = false; // 网络不可用
+        }
+        // 初始化 BasePath
+        BasePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/OneLauncher/";
+        Directory.CreateDirectory(BasePath); // 确保目录存在
+
+        // 初始化 ConfigManger
+        ConfigManger = new DBManger(new AppConfig(), BasePath);
+        Debug.WriteLine(IsNetwork);
     }
 }
