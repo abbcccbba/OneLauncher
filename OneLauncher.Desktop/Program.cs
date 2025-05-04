292469af932d6bfce52acdd17c701431fad0e79b
Lnetface <elictop@icloud.com>
2025-05-04 18:10:09 +0800

    如果一份能跑，就千万不要去动他了——把MainWindow从mvvm改了回来

--- OneLauncher.Desktop/Program.cs
+++ OneLauncher.Desktop/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.ReactiveUI;
 
@@ -11,8 +11,9 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+    BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);     
+    
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
