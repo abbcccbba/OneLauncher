0830363ac1d3ee944b0327ce56001e36edff65ea
Lnetface <elictop@icloud.com>
2025-05-18 15:28:33 +0800

    简单修改

--- OneLauncher/Views/ViewModels/AccountPageViewModel.cs
+++ OneLauncher/Views/ViewModels/AccountPageViewModel.cs
@@ -1,49 +1,48 @@
 ﻿using Avalonia.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using OneLauncher.Codes;
 using OneLauncher.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace OneLauncher.Views.ViewModels;
 internal partial class UserItem
 {
-    public string Name { get; set; }
-    public string uuid { get; set; }
-    [RelayCommand]
-    public void SetDefault(UserItem user)
-    {
-        Init.ConfigManger.config.DefaultUserModel = new UserModel
-            (Name, Guid.NewGuid());
-        Init.ConfigManger.Write(Init.ConfigManger.config);
-        MainWindow.mainwindow.ShowFlyout($"已将默认用户模型设置为{user.Name}");
-    }
+    public UserModel um { get; set; }
 }
 internal partial class AccountPageViewModel : BaseViewModel
 {
+    // 刷新
+    private void RefList()
+    {
+        UserModelList = Init.ConfigManger.config.UserModelList
+                .Select(x => new UserItem()
+                {
+                    um = x
+                }).ToList();
+    }
     public AccountPageViewModel()
     {
 #if DEBUG
         if (Design.IsDesignMode)
             UserModelList = new List<UserItem>()
             { new UserItem()
-            { 
-                Name="ZhiWei",
-                uuid="0000-0000-0000-0000-0000"
+            {
+                um = new UserModel()
+                {
+                    Name="ZhiWei",
+                    uuid=Guid.NewGuid()
+                }
             } };
         else
 #endif
-        UserModelList = Init.ConfigManger.config.UserModelList
-                .Select(x => new UserItem() 
-                { 
-                    Name = x.Name ,
-                    uuid = "UUID:"+x.uuid.ToString()
-                }).ToList();
+            RefList();
     }
     [ObservableProperty]
     public List<UserItem> _UserModelList;
@@ -59,21 +58,21 @@ internal partial class AccountPageViewModel : BaseViewModel
     [RelayCommand]
     public void Done()
     {
-        if (string.IsNullOrEmpty(UserName))
+        if (string.IsNullOrEmpty(UserName)) return;
+        if (!Regex.IsMatch(UserName, @"^[a-zA-Z0-9_]+$"))
         {
-            MainWindow.mainwindow.ShowFlyout("用户名不能为空！",true);
+            MainWindow.mainwindow.ShowFlyout("用户名包含非法字符！", true);
             return;
         }
-        UserModelList = Init.ConfigManger.config.UserModelList
-                .Select(x => new UserItem()
-                {
-                    Name = x.Name,
-                    uuid = "UUID:" + x.uuid.ToString()
-                }).ToList();
-        Init.ConfigManger.AddUserModel(new UserModel(
+        Init.ConfigManger.WriteUserModel(new UserModel(
             UserName,
             Guid.NewGuid()
         ));
+        UserModelList = Init.ConfigManger.config.UserModelList
+                .Select(x => new UserItem()
+                {
+                    um = x
+                }).ToList();
         IsPaneShow = false;
         MainWindow.mainwindow.ShowFlyout($"已新建用户:{UserName}");
     }
@@ -82,5 +81,20 @@ internal partial class AccountPageViewModel : BaseViewModel
     {
         IsPaneShow = false;
         MainWindow.mainwindow.ShowFlyout("已暂存修改！");
+    }
+    [RelayCommand]
+    public void SetDefault(UserModel user)
+    {
+        Init.ConfigManger.config.DefaultUserModel = user;
+        Init.ConfigManger.Write(Init.ConfigManger.config);
+        MainWindow.mainwindow.ShowFlyout($"已将默认用户模型设置为{user.Name}");
+    }
+    [RelayCommand]
+    public void DeleteUser(UserModel user)
+    {
+        Init.ConfigManger.config.UserModelList.Remove(user);
+        Init.ConfigManger.Write(Init.ConfigManger.config);
+        RefList();
+        MainWindow.mainwindow.ShowFlyout($"已移除用户模型{user.Name}", true);
     }
 }
